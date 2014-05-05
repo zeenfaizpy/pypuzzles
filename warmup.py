ffc3ca4a362a301fbe6719570ec996e569c67f34
Mohammad Faizal <mfaizal@deam.ae>
2014-05-05 17:43:08 +0400

    solved monkey smile puzzle

--- warmup.py
+++ warmup.py
@@ -7,4 +7,15 @@ def sleep_in(weekday, vacation):
     if not weekday or vacation:
         return True
     else:
-        return False 
+        return False
+
+def monkey_trouble(a_smile, b_smile):
+	"""
+	We have two monkeys, a and b, and the parameters a_smile and b_smile 
+	indicate if each is smiling. We are in trouble if they are both smiling
+	or if neither of them is smiling. Return True if we are in trouble.
+	"""
+	if (a_smile and b_smile) or (not a_smile and not b_smile):
+		return True
+	else:
+		return False 
